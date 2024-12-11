cf2c45000a522600142d85ec381132568c5c8530
DolaniDolani <117933408+DolaniDolani@users.noreply.github.com>
2024-12-11 20:57:30 +0100

    Add database connection
    Add
    - database connection
    - test endpoints

--- db/connection.go
+++ db/connection.go
@@ -1,12 +1,26 @@
 package db
 
 import (
-	"githubcom/jackc/pgx/v5/pgxpool"
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var Pool *pgxpool.Pool
 
 // ConnectDB create a connection to the PostgreSQL database
 func ConnectDB() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	var err error
+
+	// Create connection pool
+	Pool, err = pgxpool.New(context.Background(), databaseUrl)
+	if err != nil {
+		log.Fatalf("Could not connect do PostgreSQL database: %v", err)
+	} else {
+		log.Println("Database connected")
+	}
 
 }
--- server.go
+++ server.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/DolaniDolani/dolan-gaming/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error during loading of .env file: %v", err)
+	}
+
+	db.ConnectDB()
+
+	serverPort := os.Getenv("SERVER_PORT")
+
 	server := gin.Default()
 
 	server.GET("/test", func(ctx *gin.Context) {
@@ -11,5 +29,17 @@ func main() {
 		})
 	})
 
-	server.Run()
+	server.GET("/test-db", func(ctx *gin.Context) {
+		version := " "
+		err := db.Pool.QueryRow(ctx, "SELECT version()").Scan(&version)
+		if err != nil {
+			log.Println(err)
+			ctx.JSON(500, gin.H{"error": "Database access error"})
+			return
+		}
+		ctx.JSON(200, gin.H{"database_version": version})
+	})
+
+	server.Run(":" + serverPort)
+
 }
