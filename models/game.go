130d6943d8225fc80413c4b0f9cfbeaa9d89e147
DolaniDolani <117933408+DolaniDolani@users.noreply.github.com>
2024-12-17 00:27:16 +0100

    Add cors managament

--- models/game.go
+++ models/game.go
@@ -4,10 +4,10 @@ import "time"
 
 type Game struct {
 	ID            int64      `gorm:"primaryKey" json:"id"`
+	Name          string     `json:"name"`
 	Condition     string     `json:"condition"`
 	Console       string     `json:"console"`
 	Language      string     `json:"language"`
-	Name          string     `json:"name"`
 	Notes         string     `json:"notes"`
 	PurchaseDate  *time.Time `json:"purchase_date"`
 	PurchasePrice *float64   `json:"purchase_price"`
--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/DolaniDolani/dolan-gaming/db"
 	"github.com/DolaniDolani/dolan-gaming/routes"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -22,6 +23,14 @@ func main() {
 
 	serverPort := os.Getenv("SERVER_PORT")
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"}, // Domini autorizzati (frontend Angular)
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	routes.RegisterGameRoutes(server)
 	routes.RegisterTestRoutes(server)
